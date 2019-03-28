723309201b04baabf464dd080019cb6fd18dd875
Muriel Franco <franco@ifi.uzh.ch>
2019-03-28 19:36:31 +0100

    Configurations

---
+++ Management/Scripts/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+
+echo "Installing Squid Proxy..."
+sudo DEBIAN_FRONTEND=noninteractive apt-get install squid --yes
+
+# basic configure file
+sudo mv /etc/squid/squid.conf /etc/squid/squid.conf.original
+cd ./resources
+sudo cp squid.conf /etc/squid/squid.conf
+
+# Create initial credentials to access the proxy. username=fende and password=rnp#12345
+sudo htpasswd -cb /etc/squid/squid_passwd fende rnp#1235
---
+++ Management/Scripts/stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+sudo service squid stop
