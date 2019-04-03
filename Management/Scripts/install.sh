bbd389d3eb0b2f0a5a2a6f7578f1e4b0d64ee4e9
Giovanni Venâncio <gvs11ufpr@gmail.com>
2019-04-03 10:09:26 -0300

    Removido backup de arquivo de configuração
    Signed-off-by: Giovanni Venâncio <gvs11ufpr@gmail.com>

--- Management/Scripts/install.sh
+++ Management/Scripts/install.sh
@@ -3,12 +3,9 @@ set -e
 
 
 echo "Installing Squid Proxy..."
-
-sudo apt-get install apache2-utils
 sudo DEBIAN_FRONTEND=noninteractive apt-get install squid --yes
 
 # basic configure file
-sudo mv /etc/squid/squid.conf /etc/squid/squid.conf.original
 cd ./resources
 sudo cp squid.conf /etc/squid/squid.conf
 
