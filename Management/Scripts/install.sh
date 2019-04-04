82e980130514223d08568e5671d6d31e821acfe9
Giovanni Venâncio <gvs11ufpr@gmail.com>
2019-04-04 11:01:00 -0300

    Adicionado restart após criar nova configuração
    Signed-off-by: Giovanni Venâncio <gvs11ufpr@gmail.com>

--- Management/Scripts/install.sh
+++ Management/Scripts/install.sh
@@ -7,6 +7,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install squid --yes
 
 # basic configure file
 sudo cp /home/fende/Management/Scripts/resources/squid.conf /etc/squid/squid.conf
+sudo service squid restart
 
 # Create initial credentials to access the proxy. username=fende and password=rnp#12345
 sudo htpasswd -cb /etc/squid/squid_passwd fende rnp#1235
